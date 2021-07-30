e788253af41f33754f44dcc6fa26f1391731b40a
Stas Kravtchenko <github.learn.work@gmail.com>
2021-07-30 20:40:22 +0300

    add variable

---
+++ script.js
@@ -0,0 +1,10 @@
+alert("Привет мир");
+let money = 30;
+let income ='Доход';
+let addExpenses = 23;
+const deposit = 56;
+let mission = 176;
+const period = prompt('Что-то написать');
+console.log('money: ', money);
+console.log('income:', income);
+console.log('period', period);
